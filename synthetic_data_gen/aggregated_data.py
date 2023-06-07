c2385cbbf4ec6f87aa827d8c78d38b37b1e111a0
Sophie Parsa <sjparsa@stanford.edu>
2023-06-07 16:04:29 -0700

    code comments

--- synthetic_data_gen/aggregated_data.py
+++ synthetic_data_gen/aggregated_data.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import LogisticRegression
 import numpy as np
 from joblib import dump
 
+# Combine all train and validation data into a single dataframe each
 df_test = pd.read_csv("final_data/clinic 0/test.csv")
 df_train = pd.read_csv("final_data/clinic 0/train.csv")
 for i in range(1,15):
@@ -16,6 +17,7 @@ for i in range(1,15):
     file_name_test = "final_data/"+str("clinic ")+str(i)+"/test.csv"
     curr_df_test = pd.read_csv(file_name_test)
     df_test = pd.concat([df_test,curr_df_test])
+
 # Train shape
 print(df_train.shape)
 
@@ -25,11 +27,13 @@ print(df_test.shape)
 y = df_train['treatment'].values
 print(df_train.columns)
 X = df_train.drop(['treatment','Unnamed: 0','response_type'], axis=1).values
-#39.795*comm rounds
-#39795
+
+# Train a logistic regression classifier on aggregated train data
 clf = LogisticRegression(random_state=0, max_iter = 39795, multi_class = 'multinomial').fit(X,y)
+# Export model
 dump(clf, "logreg_baseline.joblib")
 
+# Analyzer performance on held out test data
 with open("independent_log_reg.txt",'w') as f:
     for i in range(15,25):
         file_name_test = "final_data/"+str("clinic_")+str(i)+".csv"
@@ -37,9 +41,5 @@ with open("independent_log_reg.txt",'w') as f:
         y_test = df_test['treatment'].values
         X_test = df_test.drop(['treatment','response_type'], axis=1).values
         preds = clf.predict_proba(X_test)
+        # Save predictions on each test set
         np.save("clinic_"+str(i)+"_preds.npy",preds)
-        
-        #preds_joined = ",".join(preds)
-        # f.write("clinic_"+str(i)+",")
-        # f.write(str(preds))
-        # f.write("\n")
--- synthetic_data_gen/data_split.py
+++ synthetic_data_gen/data_split.py
@@ -3,18 +3,22 @@
 import os
 import pandas as pd
 
+# Generate 80-20 train-validation splits for each clinic
 for i in range(0,25):
+    # Set up paths for the train and validation data
     folder_name = "clinic "+str(i)
     folder_path = os.path.join("final_data",folder_name)
     train_path = os.path.join(folder_path,"train.csv")
-    val_path = os.path.join(folder_path,"val.csv")
     test_path = os.path.join(folder_path,"test.csv")
     os.mkdir(folder_path)
+    # Read in current clinic
     all_data = pd.read_csv("final_data/"+"clinic_"+str(i)+".csv")
+    # Split data and print number of samples in train, test, and total
     print("Total samples: "+str(len(all_data)))
     test = all_data.sample(n=int(all_data.shape[0]*0.2), random_state=42)
     print("Test samples: "+str(len(test)))
     train = all_data.drop(test.index)
     print("Train samples: "+str(len(train)))
+    # Write the splits to new csv files
     test.to_csv(test_path)
     train.to_csv(train_path)
